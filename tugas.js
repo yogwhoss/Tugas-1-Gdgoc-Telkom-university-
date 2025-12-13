c9368080b00d5e045dfc59cee4ffda0523e7d15b
yogwhoss <yogijati052@gmail.com>
2025-12-13 17:56:55 +0700

    feat: age logic

---
+++ tugas.js
@@ -0,0 +1,35 @@
+const nama = "budi"
+const umur = 17
+
+console.log("maaf " + nama + ", umur kamu kurang " + (21 - umur) + " tahun lagi ");
+
+// const nama = "andi";
+// const umur =25;
+// const uang = 100000; 
+// Variabel diatas digunakan untuk menyimpan data nama, umur, dan uang
+
+// if (umur < 21) {
+//     console.log('maaf ' + nama + ', umur kamu kurang ' + (21 - umur ) + ' tahun lagi ');
+// } else {
+//      if (uang < 500000) {
+//         console.log('maaf ' + nama + ', uang kamu kurang 100 ribu datang lain kali');   
+//     } else {
+//      console.log('selamat datang ' + nama );
+//     }
+// }
+// if else dengan kondisi uang kurang dari 100 ribu 
+
+// const nama = "sultan";
+// const umur =25;
+// const uang = 1000000; 
+
+// if (umur < 21) {
+//     console.log('maaf ' + nama + ', umur kamu kurang ' + (21 - umur ) + ' tahun lagi ');
+// } else {
+//      if (uang < 500000) {
+//         console.log('maaf ' + nama + ', uang kamu kurang ');   
+//     } else {
+//      console.log('selamat datang, ' + nama );
+//     }
+// }
+// if else dengan kondisi sultan berhasil memasuki club di malam hari
